33ef3a0a8c2e3d5173d4fb067297ccb74b3ede69
Divyanshu <divyanshu@local>
2025-11-10 21:31:17 +0000

    Revert select-all-on-click feature due to keyboard input bugs

--- FloatyBrowser/WebViewController.swift
+++ FloatyBrowser/WebViewController.swift
@@ -483,13 +483,6 @@ class WebViewController: NSViewController {
 // MARK: - NSTextFieldDelegate
 
 extension WebViewController: NSTextFieldDelegate {
-    func controlTextDidBeginEditing(_ obj: Notification) {
-        // Select all text when user clicks in the URL field
-        if let textField = obj.object as? NSTextField, textField == urlField {
-            textField.currentEditor()?.selectAll(nil)
-        }
-    }
-    
     func control(_ control: NSControl, textView: NSTextView, doCommandBy commandSelector: Selector) -> Bool {
         if commandSelector == #selector(NSResponder.insertNewline(_:)) {
             // User pressed Enter
